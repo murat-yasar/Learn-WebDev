5a0b9c88a965a05fd6fbdfa91df35c134439c0fd
Murat Yaşar <murat.yasar@comkom.de>
2024-07-05 08:31:54 +0200

    Structure working tree

--- Coursera/UMichigan/3-Javascript/js/script.js
+++ Coursera/UMichigan/3-Javascript/js/script.js
@@ -1,9 +1,3 @@
-/*
-const photos = [
-  ["home-office.jpg", "laptop.jpg", "meeting.jpg", "office.jpg", "strategy.jpg", "web-design.jpg"],
-  ["athens.jpg", "aztec.jpg", "cappadocia.jpg", "great-wall.jpg", "kremlin.jpg", "kyoto.jpg", "paris.jpg", "petra.jpg", "pisa.jpg", "pyramid.jpg", "taj-mahal.jpg", "venice.jpg"]
-]
-*/
 
 const photos = {
   work: ["home-office.jpg", "laptop.jpg", "meeting.jpg", "office.jpg", "strategy.jpg", "web-design.jpg"],

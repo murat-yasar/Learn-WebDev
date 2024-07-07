36778a722a36e863b2d5889390ede3f271a21547
Murat Yaşar <murat.yasar@comkom.de>
2024-07-07 16:21:56 +0200

    Learn Media-Queries

--- Coursera/UMichigan/4-Responsive-Design/js/script.js
+++ Coursera/UMichigan/4-Responsive-Design/js/script.js
@@ -1,1 +1,14 @@
 console.log('JS initiated!');
+
+images = [
+  "aztec.jpg",
+  "cappadocia.jpg",
+  "great-wall.jpg",
+  "kremlin.jpg",
+  "kyoto.jpg",
+  "paris.jpg",
+  "pisa.jpg",
+  "pyramid.jpg",
+  "taj-mahal.jpg",
+  "venice.jpg",
+];

9dd3c7eaea843a9f5a4fb61d6f423010e76d4f1e
Murat Yaşar <murat.yasar@comkom.de>
2024-08-22 11:42:00 +0200

    Learn: Variables

---
+++ YouTube/PHP-MySQL/index.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1); ?>
+
+<!DOCTYPE html>
+<html lang='EN'>
+<head>
+<meta charset='UTF-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'>
+<link rel='stylesheet' href='css/styles.css'></link>
+<title>Document</title>
+
+</head>
+<body>
+
+<div class="container-fluid">
+   <div class="alert alert-primary mt-5">
+      <?php echo "PHP"; ?>
+   </div>
+</div>
+
+
+<script src='js/script.js'></script>
+<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'></script>
+</body>
+</html>

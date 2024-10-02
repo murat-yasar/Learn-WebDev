12ea1c2a787da0512493d0ad37702574d67cf912
Murat Yaşar <murat.yasar@comkom.de>
2024-10-02 12:58:47 +0200

    Add: Calc class

---
+++ PHP-OOP/classes/Calc.class.php
@@ -0,0 +1,29 @@
+<?php
+
+class Calc {
+   public $num1;
+   public $num2;
+   public $operator;
+
+   public function __construct(int $num1, int $num2, string $opr){
+      $this->num1 = $num1;
+      $this->num2 = $num2;
+      $this->opr = $opr;
+   }
+
+   public function calculate() : int {
+      switch ($this->operator) {
+         case 'add':
+            return $this->num1 + $this->num2;
+         case 'sub':
+            return $this->num1 - $this->num2;
+         case 'mult':
+            return $this->num1 * $this->num2;
+         case 'div':
+            return $this->num1 / $this->num2;
+         default:
+            echo "ERROR! $this->operator is not a valid operator!";
+            break;
+      }
+   }
+}

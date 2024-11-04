09e5dd7e92bc4ec0ef147225eae0439f2867e1c3
Murat Yaşar <murat.yasar@comkom.de>
2024-11-04 17:44:13 +0100

    Create: pdo.php for mysql connection

--- Coursera/UMichigan/8-DB-app-PHP/index.php
+++ Coursera/UMichigan/8-DB-app-PHP/index.php
@@ -1,11 +1,12 @@
 <?php
-echo "<pre>";
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc', 'root', 'root');
+require_once 'pdo.php';
+
 $stmt = $pdo->query("SELECT * FROM users");
 
+echo "<table border=1><tr><td>ID</td><td>User Name</td><td>User Email</td></tr>";
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-   print_r($row);
+   echo "<tr><td>$row[user_id]</td><td>$row[user_name]</td><td>$row[user_email]</td></tr>";
 }
-echo "<pre>";
+echo "</table>";
 
 ?>

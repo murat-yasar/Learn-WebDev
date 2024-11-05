6ead08ea26887d1030ef36c9604afe274ac137e3
Murat Yaşar <murat.yasar@comkom.de>
2024-11-05 11:20:40 +0100

    Add: add.php

---
+++ Coursera/UMichigan/8-DB-app-PHP/add.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'pdo.php';
+
+if ($_POST['user_password1'] === $_POST['user_password2']) {
+   $_POST['user_password'] = $_POST['user_password1'];
+} else {
+   echo "<p style='color:red;'>Passwords do not match!</p>";
+   echo "<a href='index.php'>Back</a>";
+   exit;
+}
+
+if(isset($_POST['user_name']) && isset($_POST['user_email']) && isset($_POST['user_password'])) 
+{
+   $sql = "INSERT INTO users (user_name, user_email, user_password) VALUES (:user_name, :user_email, :user_password)";
+
+   echo "<pre>$sql</pre>";
+
+   $stmt = $pdo->prepare($sql);
+   $stmt->execute(
+      array(
+         ':user_name' => $_POST['user_name'], 
+         ':user_email' => $_POST['user_email'], 
+         ':user_password' => $_POST['user_password']
+      ));
+   header("Location: index.php");
+}
--- Coursera/UMichigan/8-DB-app-PHP/index.php
+++ Coursera/UMichigan/8-DB-app-PHP/index.php
@@ -8,5 +8,41 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
    echo "<tr><td>$row[user_id]</td><td>$row[user_name]</td><td>$row[user_email]</td></tr>";
 }
 echo "</table>";
-
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>PHP (MVC) & MySQL (PDO)</title>
+</head>
+<body>
+   <h1>Add a New User</h1>
+   <form method="post" action="add.php">
+      <table>
+         <tr>
+            <td>Name: </td>
+            <td><input type="text" name="user_name"></td>
+         </tr>
+         <tr>
+            <td>E-mail: </td>
+            <td><input type="email" name="user_email"></td>
+         </tr>
+         <tr>
+            <td>Password: </td>
+            <td><input type="password" name="user_password1"></td>
+         </tr>
+         <tr>
+            <td>Repeat Password: </td>
+            <td><input type="password" name="user_password2"></td>
+         </tr>
+         <tr>
+            <td><input type="submit" value="Add"></td>
+            <td></td>
+         </tr>
+      </table>
+   </form>
+</body>
+</html>

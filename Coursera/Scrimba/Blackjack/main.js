6675f61c3e74bd43918b8adc0dd0bf41652dd0c4
Murat Yaşar <dev.muratyasar@gmail.com>
2025-03-05 08:35:41 +0100

    Fix: start button

--- Coursera/Scrimba/Blackjack/main.js
+++ Coursera/Scrimba/Blackjack/main.js
@@ -1,12 +1,34 @@
-let firstCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2; 
-let secondCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
-let sum = firstCard + secondCard;
+// Selectors
+let messageEl = document.querySelector("#messageEl");
+let cardsEl = document.querySelector("#cardsEl");
+let sumEl = document.querySelector("#sumEl");
+let startButton = document.querySelector("#startButton");
+
+// Variables
 let hasBlackJack = false;
 let isAlive = true;
-let message = "";
+let message = "Want to play a round?";
+let cards = []; 
+let sum = 0;
 
+// Functions
+let getRandomCard = () =>  Math.floor(Math.random() * (11 - 2 + 1)) + 2;
+
+let buildBoard = () => {
+   cards = [];
+   cards.push(getRandomCard());
+   cards.push(getRandomCard());
+   sum = cards.reduce((a, b) => a + b, 0);
+}
 
 function startGame() {
+   buildBoard();
+   renderGame();
+}
+
+function renderGame() {
+   cardsEl.textContent = "Cards: ";
+
    if (sum === 21) {
       hasBlackJack = true;
       message = "You win! Blackjack!";
@@ -17,7 +39,13 @@ function startGame() {
       isAlive = false;
    }
 
-   console.log(message);
+   cards.forEach((card) => {cardsEl.textContent += card + " ";});
+   messageEl.textContent = message;
+   sumEl.textContent = "Sum: " + sum;
 }
 
-startGame();
+let newCard = () => {
+   cards.push(getRandomCard());
+   sum += cards[cards.length - 1];
+   renderGame();
+}

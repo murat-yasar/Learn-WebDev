a4b5e3b55556a254da7c8e165b13ba3ca2de0f1a
Murat Yaşar <dev.muratyasar@gmail.com>
2025-03-10 08:42:59 +0100

    Feat: Store data in local storage

--- Coursera/Scrimba/chrome-extension/script.js
+++ Coursera/Scrimba/chrome-extension/script.js
@@ -1,19 +1,15 @@
-// Elements
+//* Elements
 const inputField = document.querySelector("#input-el");
 const saveBtn = document.querySelector("#btn-save");
 const listField = document.querySelector("#list-el");
 
-// Variables
+
+//* Variables
 let links = [];
 
-// Events
-saveBtn.addEventListener("click", ()=>{
-   links.push(inputField.value);
-   inputField.value = "";
-   
-   renderList();
-});
 
+//* Events
+// Trigger SAVE button by pressing enter key on keyboard
 inputField.addEventListener("keypress", ()=> {
    if (event.key === "Enter") {
       event.preventDefault();
@@ -21,10 +17,31 @@ inputField.addEventListener("keypress", ()=> {
    }
 });
 
+// Save by clicking SAVE button
+saveBtn.addEventListener("click", ()=>{
+   if (inputField.value) {
+      // Reset input-field after saving
+      links.push(inputField.value);
+      inputField.value = "";
+
+      storeData();
+      renderList();
+   }
+});
+
 
 // Functions
+// Store data in localStorage
+const storeData = () => {
+   localStorage.setItem("favLinks", JSON.stringify(links));
+   console.log(localStorage.getItem("favLinks"));
+}
+
+// Print data on browser
 const renderList = () => {
-   itemList = "";
+   let itemList = "";
+
+   links = JSON.parse(localStorage.getItem("favLinks"));
    
    links.forEach(el => {
       itemList += `<li><a href="${el}" target="_blank">${el}</a></li>`;

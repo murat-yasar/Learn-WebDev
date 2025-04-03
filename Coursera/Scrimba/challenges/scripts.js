a9264ebac9b770009d88575249f8e4fc7196f508
Murat Yaşar <dev.muratyasar@gmail.com>
2025-04-03 20:58:59 +0200

    Solve: 06.Emojify

--- Coursera/Scrimba/challenges/scripts.js
+++ Coursera/Scrimba/challenges/scripts.js
@@ -1,4 +1,4 @@
-/* Panic function 
+/* //* 01. Panic function 
 Write a PANIC! function. The function should take in a sentence and return the same
 sentence in all caps with an exclamation point (!) at the end. Use JavaScript's
 built in string methods. 
@@ -25,7 +25,7 @@ console.log(panic("winter is coming"))
 
 
 
-/* Whispering function 
+/* //* 02. Whispering function 
 Write a function `whisper` that takes in a sentence 
 and returns a new sentence in all lowercase letters with
 "shh..." at the beginning. 
@@ -51,7 +51,7 @@ console.log(whisper("MA'AM, this is a Wendy's!"));
 
 
 
-/* Alternating Caps 
+/* //* 03. Alternating Caps 
  Write a function that takes in a string of letters
  and returns a sentence in which every other letter is capitalized.
 
@@ -76,7 +76,7 @@ console.log(altCaps("When you visit Portland you have to go to VooDoo Donuts"));
 
 
 
-/* toTitleCase
+/* //* 04. toTitleCase()
 Write a function that will capitalize every word in a sentence.  
 
 Example Input: "everything, everywhere, all at once"
@@ -99,7 +99,7 @@ console.log(toTitleCase("pumpkin pranced purposefully across the pond"));
 
 
 
-/* Totally Not Another FizzBuzz 
+/* //* 05. Totally Not Another FizzBuzz 
 Scrimba CEO Per Borgen wants you to write a program to grant special bonuses to all his employees based on their employee ID numbers! 
 
 Scrimba has 100 employees and their employee ID numbers range from 1 - 100. If the employee's ID number is: 
@@ -133,5 +133,81 @@ function awardBonuses(){
 }
 
 awardBonuses();
+
+
+
+/*  //* 06. Emojify!
+Popular services like Slack and Github allow for emoji shortscodes, meaning 
+they will detect when a word in a sentence begins and ends with a colon (:)
+and automatically replace that word with an emoji. 
+
+These shortcodes allow users to add an emoji to their messages by typing a 
+code rather than searching for an emoji from a list. 
+
+For example, typing :smile: will replace that text with 😊 
+
+*/
+
+const emojis = {
+    "smile": "😊",
+    "angry": "😠",
+    "party": "🎉",
+    "heart": "💜",
+    "cat":   "🐱",
+    "dog":   "🐕"
+}
+
+/* 1. Write a function that checks if a lowercase word starts and 
+ends with a colon. If it does, remove the colons and
+look up the word in the emoji object. If the word is in the 
+emojis object, return the corresponding emoji.
+If it isn't, return the original word.
+
+Example input: ":party:"
+Example output: 🎉
+
+Example input: ":flower:"
+Example output: "flower"
+
+Example input: "elephant"
+Example output: "elephant"
+*/ 
+
+function emojifyWord(word){
+    if(!word.startsWith(":") && !word.endsWith(":")) return word; 
+    
+    const slice = word.slice(1, -1);
+
+    if(emojis[slice]){
+         return emojis[slice]
+    } else {
+        return slice;
+    }   
+}
+
+/* 2. Write a function to find any emoji shortcodes in a phrase.
+Your function should map over each word in the phrase, emojify any word
+that begins and ends with a colon, then return the emojified phrase. 
+Feel free to use your emojify function from the previous exercise!
+
+Example input: "I :heart: my :cat:"
+Example output: "I 💜 my 🐱"
+
+Example input: "I :heart: my elephant"
+Example output: "I 💜 my elephant"
+*/ 
+
+function emojifyPhrase(phrase){
+    const newPhrase = phrase.split(" ").map(word => emojifyWord(word));
+    return newPhrase.join(" ")
+    
+}
+
+// Test Cases
+console.log(emojifyWord(":heart:"));
+console.log(emojifyWord(":flower:"));
+console.log(emojifyWord("elephant"));
+console.log(emojifyPhrase("I :heart: my :cat:"));
+console.log(emojifyPhrase("I :heart: my :elephant:"));
 
 

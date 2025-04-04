536f6e130e86887d8e28101b04c9c8c5741991e2
Murat Yaşar <dev.muratyasar@gmail.com>
2025-04-04 08:50:12 +0200

    Solve: 11.Frequency of Letters in Your Name

--- Coursera/Scrimba/challenges/scripts.js
+++ Coursera/Scrimba/challenges/scripts.js
@@ -360,7 +360,44 @@ function removeDupeChars(chars){
     return sanitizedPW;
 }
 
+// Test Cases
 console.log(removeDupeChars("aabbccb1212"));
 console.log(removeDupeChars(password));
 
 
+
+/* //* 11. Frequency of Letters in Your Name
+How often do the letters in your name repeat? 
+
+Write a function that counts how many times each letter of your name
+occurs. Your function should take in your first and last name and return
+an object where the keys are each character in your name, and the value
+is how many times that character appears in your name. 
+
+Example input: "Peggy Porth"
+Example output: {p: 2, e: 1, g: 2, y: 1, o: 1, r: 1, t: 1, h: 1}
+
+Your function should NOT count spaces and should not be case sensitive (a
+lowercase t and a capital T should be considered the same character).
+
+*/ 
+
+function countChars(str){
+    let output = {};
+    let strArr = str.toLowerCase().split("");
+    
+    strArr.forEach(item => {
+        if (item === " ") return; // skip spaces
+
+        if (output[item]) {
+            output[item]++;
+        } else {
+            output[item] = 1;
+        }
+    });
+    
+    return output;
+}
+
+// Test Cases
+console.log(countChars("Peggy Porth"));

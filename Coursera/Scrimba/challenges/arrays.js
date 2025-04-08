9272602472522f08f3fed53c70d2fa225c193cc4
Murat Yaşar <dev.muratyasar@gmail.com>
2025-04-08 13:38:02 +0200

    Solve: 03 - Count the Scrimba Students

--- Coursera/Scrimba/challenges/arrays.js
+++ Coursera/Scrimba/challenges/arrays.js
@@ -95,3 +95,34 @@ function flatten(arr){
 console.log("02 - Pumpkin's Prizes:");
 console.log(flatten(kittyPrizes));
 console.log(flatten(kittyScores));
+
+
+
+/* //* 03 - Count the Scrimba Students
+Alex from Scrimba wants to know how many new students have attended 
+Scrimba's weekly Town Hall event this year. 
+
+He has an array of first-time attendees for each month of the year. 
+Help him find the total number of attendees! Your function should
+take in an array and return a number representing the total number
+of new attendees. 
+
+Example input: [1,2,3]
+Example output: 6
+ */
+
+const studentCount = [50,53,61,67,60,70,78,80,80,81,90,110];
+
+function sumArray(arr){
+   // Solution.1
+   // return arr.reduce((acc, curr) => acc + curr, 0);
+
+   // Solution.2
+   let total = 0;
+   arr.forEach(el => total += el);
+   return total;
+}
+
+// Test Cases
+console.log("03 - Count the Scrimba Students:");
+console.log(sumArray(studentCount));

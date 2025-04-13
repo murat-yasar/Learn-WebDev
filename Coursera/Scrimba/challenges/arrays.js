c9969fdbb07fb8cfc97c9a66b11637840922a05d
Murat Yaşar <dev.muratyasar@gmail.com>
2025-04-13 14:37:18 +0200

    Solve: 04 - Pizza Night?

--- Coursera/Scrimba/challenges/arrays.js
+++ Coursera/Scrimba/challenges/arrays.js
@@ -126,3 +126,43 @@ function sumArray(arr){
 // Test Cases
 console.log("03 - Count the Scrimba Students:");
 console.log(sumArray(studentCount));
+
+
+
+/* //* 04 - Pizza Night? 
+It's the weekend and you and your friends can't agree on 
+what to order for dinner, so you put it to a vote. 
+
+Write a function to find the food with the highest number of votes. 
+
+Your function should take in a food object and find the food
+with the most votes. It should log the winner, along with 
+how many votes it received.  
+
+Example input: {"🐈 cats": 19, "🐕 dogs": 17} 
+Example output: The winner is 🐈 cats with 19 votes!
+*/ 
+
+const gameNightFood = {
+   "🍕 pizza": 3, 
+   "🌮 tacos": 10, 
+   "🥗 salads": 7,
+   "🍝 pasta": 5
+}
+
+function findTheWinner(obj){
+   let winKey = "";
+   let winVal = 0;
+   
+   for (const [key, val] of Object.entries(obj)){
+       if (val >= winVal) {
+           winKey = key;
+           winVal = val;
+       }
+   }
+   
+   return `The winner is ${winKey} with ${winVal} votes!`;
+}
+
+// Test Cases
+console.log(findTheWinner(gameNightFood));

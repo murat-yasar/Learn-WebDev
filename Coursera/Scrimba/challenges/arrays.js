91a0a9b94f2626caf40ea93455d626a802a38f2e
Murat Yaşar <dev.muratyasar@gmail.com>
2025-04-15 12:34:12 +0200

    Solve: 07 - Candy Sale

--- Coursera/Scrimba/challenges/arrays.js
+++ Coursera/Scrimba/challenges/arrays.js
@@ -256,3 +256,38 @@ function getFreePodcasts(data){
 console.log(getFreePodcasts(podcasts))
 
 
+
+/* //* 07 - Candy Sale
+   It's the day after Halloween 🎃 and all the candy is on sale!
+   
+   To buy up all the candy, use map() and filter() to put all the
+   candy into a `shoppingCart` array. 
+   
+   The new array should contain only the item and the price, like
+   this: 
+   
+   Expected output: 
+   [
+       {item: "🍭", price: 2.99},
+       {item: "🍫", price: 1.99}, 
+       {item: "🍬", price: 0.89}
+    ]
+*/
+import products from "./assets/data-07.js";
+
+function getSaleItems(data){
+    return data
+            .filter(item => item.type === "sweet")
+            .map(sweet => {
+                return {
+                    item: sweet.item,
+                    price: sweet.price
+                }
+            })
+};
+
+// Test Cases
+console.log(getSaleItems(products));
+
+
+

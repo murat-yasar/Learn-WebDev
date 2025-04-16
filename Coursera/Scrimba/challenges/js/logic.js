16bcdec4e94a8bd423306dd25e48c694f157acff
Murat Yaşar <dev.muratyasar@gmail.com>
2025-04-16 07:56:59 +0200

    Solve: 01 - Total Savory Items

---
+++ Coursera/Scrimba/challenges/js/logic.js
@@ -0,0 +1,17 @@
+/*  //* 01 - Total Savory Items
+Use reduce() to total the groceries. 
+Then find a method that will round the total to 2 decimal places.
+
+Example output: 73.44
+*/
+import shoppingCart from "assets/data-07.js";
+
+function total(arr){
+    const total = arr.reduce((acc, curr) => {
+        return acc + curr.price;
+    }, 0);
+    
+   return total.toFixed(2);
+}
+
+console.log(total(shoppingCart));

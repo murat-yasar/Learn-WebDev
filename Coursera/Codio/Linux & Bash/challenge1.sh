145540ca79c546b665ca40a67398b2dfb515ec8e
Murat Yaşar <dev.muratyasar@gmail.com>
2025-04-16 17:50:42 +0200

    Solve: bash challenges 1 and 2

---
+++ Coursera/Codio/Linux & Bash/challenge1.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "Hello from user: $USER"
+echo "Our home directory is: $HOME"
+echo "The shell we're using is: $SHELL"
+echo "Our current working directory is: $PWD"
+source commands2.txt

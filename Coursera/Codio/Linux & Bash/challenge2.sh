5931fd34620ee297fe7bdbba4159e3ce77f0a36c
Murat Yaşar <dev.muratyasar@gmail.com>
2025-04-23 10:25:00 +0200

    Solve: Challenge2

--- Coursera/Codio/Linux & Bash/challenge2.sh
+++ Coursera/Codio/Linux & Bash/challenge2.sh
@@ -2,6 +2,6 @@
 
 echo "Reporting for user: $USER" > introduction.txt
 echo "The home directory is: $HOME" >> introduction.txt
-ls /notReally 1> output.txt 2> error.txt
+ls /notReally 1>output.txt 2>error.txt
 sort < champs.txt > alpha-winners.txt
 cat alpha-winners.txt | less

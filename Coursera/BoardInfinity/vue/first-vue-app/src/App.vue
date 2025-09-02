87549065eac2023d2628973e74ad5904ed9ea4bb
Murat Yaşar <dev.muratyasar@gmail.com>
2025-09-02 17:15:35 +0200

    Add: image to the slot

--- Coursera/BoardInfinity/vue/first-vue-app/src/App.vue
+++ Coursera/BoardInfinity/vue/first-vue-app/src/App.vue
@@ -2,18 +2,24 @@
   <!-- Counter Example -->
   <Counter />
 
-  <hr><br>
-
+  <br><hr><br>
   <!-- Props Example -->
   <Greet name="Murat" />
   <Greet name="Memluk" />
   <Greet :name="name" />
 
-  <hr><br>
-
+  <br><hr><br>
   <!-- Emitter Example -->
   <button @click="isPopupVisible=true">Show Popup</button>
   <Popup v-if="isPopupVisible" @close="isPopupVisible=false" />
+
+  <br><hr><br>
+  <!-- Slot Example -->
+  <Card><h2>Card Component (Slots)</h2></Card>
+  <card><img src="../src/assets/logo.png" /></card>
+
+  <br><hr><br>
+  <!-- Slot Example -->
 
 </template>
 
@@ -21,13 +27,15 @@
 import Counter from './components/Counter.vue';
 import Greet from './components/Greet.vue';
 import Popup from './components/Popup.vue';
+import Card from './components/Card.vue';
 
 export default {
   name: 'App',
   components: {
     Counter,
     Greet,
-    Popup
+    Popup,
+    Card
   },
   data(){
     return {
---
+++ Coursera/BoardInfinity/vue/first-vue-app/src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+   <div class="card">
+      <slot></slot>
+   </div>
+</template>
+
+<script>
+   export default {
+      name: 'Card'
+   }
+</script>
+
+<style scoped>
+   .card {
+      box-shadow: 0 4px 8px 0 rgb(0,0,0,.2);
+      padding: 10px;
+      margin: 10px;
+      width: 400px;
+   }
+</style>

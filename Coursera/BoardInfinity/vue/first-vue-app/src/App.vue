c2e61093103fab2f2130c71f8eaad6a9b2c1750b
Murat Yaşar <dev.muratyasar@gmail.com>
2025-09-02 12:39:43 +0200

    Add methods to counter

--- Coursera/BoardInfinity/vue/first-vue-app/src/App.vue
+++ Coursera/BoardInfinity/vue/first-vue-app/src/App.vue
@@ -1,29 +1,50 @@
 <template>
   <table>
     <thead>
-      <tr>
-        <th></th>
-        <th><h2>Counter</h2></th>
-        <th></th>
-      </tr>
+      <tr><th></th><th><h2>Counter Inline</h2></th><th></th></tr>
     </thead>
     <tbody>
       <tr>
-        <td><button @click="counter-=1">Decrease</button></td>
+        <td><button v-on:click="counter1-=1">Decrease</button></td>
         <td style="text-align: center;">
-          <span v-bind:style="{ fontSize: '2rem'}">{{ counter }}</span>
+          <span v-bind:style="{ fontSize: '2rem'}">{{ counter1 }}</span>
         </td>
-        <td><button v-on:click="counter+=1">Increase</button></td>
+        <td><button @click="counter1+=1">Increase</button></td>
       </tr>
       <tr>
         <td></td>
         <td style="text-align: center;">
-          <button v-on:click="counter=0">Reset</button>
+          <button @click="counter1=0">Reset</button>
         </td>
         <td></td>
       </tr>
     </tbody>
   </table>
+
+  <br><hr><br>
+
+   <table>
+    <thead>
+      <tr><th></th><th><h2>Counter with Method</h2></th><th></th></tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><button @click="decrease(1)">Decrease</button></td>
+        <td style="text-align: center;"><span v-bind:style="{ fontSize: '2rem'}">{{ counter2 }}</span></td>
+        <td><button @click="increase(1)">Increase</button></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td style="text-align: center;"><button @click="reset()">Reset</button></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+
+  <br><hr><br>
+
+
+
 </template>
 
 <script>
@@ -32,8 +53,20 @@ export default {
   name: 'App',
   data() {
     return {
-      counter: 0,
+      counter1: 0,
+      counter2: 0
     }
+  },
+  methods: {
+    increase(num){
+      this.counter2 += num
+    },
+    decrease(num){
+      this.counter2 -= num
+    },
+    reset() {
+      this.counter2 = 0
+    } 
   }
 }
 </script>

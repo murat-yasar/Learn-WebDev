5b05d22b2bbc63632d932602cb4f518bcf87b817
Murat Yaşar <dev.muratyasar@gmail.com>
2025-09-02 14:50:06 +0200

    Learn: props and emits

--- Coursera/BoardInfinity/vue/first-vue-app/src/App.vue
+++ Coursera/BoardInfinity/vue/first-vue-app/src/App.vue
@@ -2,19 +2,29 @@
   <Greet name="Murat" />
   <Greet name="Memluk" />
   <Greet :name="name" />
+
+  <hr><br>
+
+  <!-- Emitter Example -->
+   <button @click="isPopupVisible=true">Show Popup</button>
+  <Popup v-if="isPopupVisible" @close="isPopupVisible=false" />
+
 </template>
 
 <script>
 import Greet from './components/Greet.vue';
+import Popup from './components/Popup.vue';
 
 export default {
   name: 'App',
   components: {
-    Greet
+    Greet,
+    Popup
   },
-  data() {
+  data(){
     return {
-      name: 'SuperCoder'
+      name: 'SuperCoder',
+      isPopupVisible: false
     }
   }
 }
---
+++ Coursera/BoardInfinity/vue/first-vue-app/src/components/Popup.vue
@@ -0,0 +1,11 @@
+<template>
+   <h2>Here is a Popup!</h2>
+   <button @click="$emit('close')">Close</button>
+</template>
+
+<script>
+   export default {
+      name: 'Popup',
+      emits: ['close']
+   }
+</script>

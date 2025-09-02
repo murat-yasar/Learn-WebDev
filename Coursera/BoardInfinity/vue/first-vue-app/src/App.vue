73d1d0e8721b7653a746ee022ba671323a5c2b27
Murat Yaşar <dev.muratyasar@gmail.com>
2025-09-02 16:54:59 +0200

    Add Counter as component

--- Coursera/BoardInfinity/vue/first-vue-app/src/App.vue
+++ Coursera/BoardInfinity/vue/first-vue-app/src/App.vue
@@ -1,4 +1,10 @@
 <template>
+  <!-- Counter Example -->
+  <Counter />
+
+  <hr><br>
+
+  <!-- Props Example -->
   <Greet name="Murat" />
   <Greet name="Memluk" />
   <Greet :name="name" />
@@ -6,18 +12,20 @@
   <hr><br>
 
   <!-- Emitter Example -->
-   <button @click="isPopupVisible=true">Show Popup</button>
+  <button @click="isPopupVisible=true">Show Popup</button>
   <Popup v-if="isPopupVisible" @close="isPopupVisible=false" />
 
 </template>
 
 <script>
+import Counter from './components/Counter.vue';
 import Greet from './components/Greet.vue';
 import Popup from './components/Popup.vue';
 
 export default {
   name: 'App',
   components: {
+    Counter,
     Greet,
     Popup
   },
---
+++ Coursera/BoardInfinity/vue/first-vue-app/src/components/Counter.vue
@@ -0,0 +1,42 @@
+<template>
+   <table>
+    <thead>
+      <tr><th></th><th><h2>Counter with Method</h2></th><th></th></tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><button @click="decrease(1)">Decrease</button></td>
+        <td style="text-align: center;"><span v-bind:style="{ fontSize: '2rem'}">{{ counter }}</span></td>
+        <td><button @click="increase(1)">Increase</button></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td style="text-align: center;"><button @click="reset()">Reset</button></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+
+export default {
+  name: 'Counter',
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increase(num){
+      this.counter += num
+    },
+    decrease(num){
+      this.counter -= num
+    },
+    reset() {
+      this.counter = 0
+    } 
+  }
+}
+</script>

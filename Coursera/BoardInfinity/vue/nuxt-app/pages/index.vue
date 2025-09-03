3267bc6fdd344bcd6aae99ddabef23106c7c959b
Murat Yaşar <dev.muratyasar@gmail.com>
2025-09-03 17:56:26 +0200

    Learn: fetching data from api

--- Coursera/BoardInfinity/vue/nuxt-app/pages/index.vue
+++ Coursera/BoardInfinity/vue/nuxt-app/pages/index.vue
@@ -4,13 +4,21 @@
    <!-- <HomepageBanner /> -->
    <!-- <HomepageCard /> -->
 
+   <!--
    <h1>{{ counter }}</h1>
    <button @click="dec">dec</button>
    <button @click="inc">inc</button>
+   -->
+
+   <div class="card" v-for="item in products">
+      <h3>{{ item.title }}</h3>
+      <p>{{ item.description }}</p>
+   </div>
 
 </template>
 
-<script>
+<script setup>
+   /*
    export default {
       data(){
          return {
@@ -26,4 +34,14 @@
          }
       }
    }
+   */
+  const { data:products } = await useFetch('https://fakestoreapi.com/products');
 </script>
+
+<style>
+.card {
+   border: 1px solid black;
+   padding: 10px;
+   margin: 5px;
+}
+</style>

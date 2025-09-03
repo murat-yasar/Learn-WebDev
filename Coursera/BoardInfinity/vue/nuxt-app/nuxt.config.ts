b322c7654a6e47b5e37a84d25dfb33a880bf4a83
Murat Yaşar <dev.muratyasar@gmail.com>
2025-09-03 22:57:11 +0200

    Add: metatags

--- Coursera/BoardInfinity/vue/nuxt-app/nuxt.config.ts
+++ Coursera/BoardInfinity/vue/nuxt-app/nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'My first Nuxt App',
+      meta: [
+        {name: 'description', content: 'Description about my webpage'},
+        {name: 'tags', content: 'Nuxt, SEO, JS'}
+      ],
+      link: [
+        {rel: 'stylesheet', href: 'https://fonts.googleapis.com'}
+      ]
+    }
+  }
 })

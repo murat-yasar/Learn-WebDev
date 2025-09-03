03f409e34ff9d41ddfec3dfd8081f1245a592357
Murat Yaşar <dev.muratyasar@gmail.com>
2025-09-03 10:01:05 +0200

    Add: NuxtLink

--- Coursera/BoardInfinity/vue/nuxt-app/layouts/default.vue
+++ Coursera/BoardInfinity/vue/nuxt-app/layouts/default.vue
@@ -1,5 +1,13 @@
 <template>
-   <header class="header"><h1>Main Header</h1></header>
+   <header class="header">
+      <nav>
+         <ul>
+            <li class="nav-link"><NuxtLink to="/">Home</NuxtLink></li>
+            <li class="nav-link"><NuxtLink to="/about">About</NuxtLink></li>
+            <li class="nav-link"><NuxtLink to="/products">Products</NuxtLink></li>
+         </ul>
+      </nav>
+   </header>
 
    <slot />
 </template>
@@ -8,5 +16,11 @@
 .header {
    background-color: black;
    color: white;
+}
+.nav-link {
+   display: inline-block;
+   text-decoration: none;
+   color: white;
+   margin: 10px;
 }
 </style>
--- Coursera/BoardInfinity/vue/nuxt-app/pages/products/index.vue
+++ Coursera/BoardInfinity/vue/nuxt-app/pages/products/index.vue
@@ -1,9 +1,3 @@
 <template>
    <h1>Products</h1>
 </template>
-
-<script>
-   definePageMeta({
-      layout: 'products'
-   })
-</script>

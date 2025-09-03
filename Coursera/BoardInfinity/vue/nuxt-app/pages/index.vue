a23be5812670904dfb85717ada82228740d34d81
Murat Yaşar <dev.muratyasar@gmail.com>
2025-09-03 17:12:48 +0200

    Add: counter

--- Coursera/BoardInfinity/vue/nuxt-app/pages/index.vue
+++ Coursera/BoardInfinity/vue/nuxt-app/pages/index.vue
@@ -1,7 +1,29 @@
 <template>
    <h1>Home</h1>
 
-   <HomepageBanner />
-   <HomepageCard />
+   <!-- <HomepageBanner /> -->
+   <!-- <HomepageCard /> -->
+
+   <h1>{{ counter }}</h1>
+   <button @click="dec">dec</button>
+   <button @click="inc">inc</button>
 
 </template>
+
+<script>
+   export default {
+      data(){
+         return {
+            counter: 0
+         }
+      },
+      methods: {
+         inc(){
+            this.counter++
+         },
+         dec(){
+            this.counter--
+         }
+      }
+   }
+</script>

bf9b68c810d369f7f6f106da7694e8562844255b
Murat Yaşar <dev.muratyasar@gmail.com>
2025-09-03 09:16:24 +0200

    Add: components

---
+++ Coursera/BoardInfinity/vue/nuxt-app/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+   <div class="card">
+      <h3>Product Name</h3>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque in minus perferendis dolor dignissimos enim veritatis. Placeat voluptas ex excepturi laboriosam voluptatum reprehenderit optio neque cum illum aperiam! Laudantium, velit!</p>
+      <button class="button">Buy</button>
+   </div>
+</template>
+
+<style>
+.card {
+   padding: 5px;
+   border-radius: 5px;
+   border: 1px solid black;
+   width: 25%;
+   display: inline-block;
+   margin: 5px;
+}
+.button {
+   padding: 5px;
+   color: white;
+   background-color: dodgerblue;
+   border: none;
+}
+</style>
--- Coursera/BoardInfinity/vue/nuxt-app/pages/index.vue
+++ Coursera/BoardInfinity/vue/nuxt-app/pages/index.vue
@@ -1,3 +1,12 @@
 <template>
    <h1>Home</h1>
+
+   <Card />
+   <Card />
+   <Card />
+   <Card />
+   <Card />
+   <Card />
+   <Card />
+
 </template>

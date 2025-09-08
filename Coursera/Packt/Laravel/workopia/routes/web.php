ef8e0b7b9abb5f290184f78f8e18916663905139
Murat Yaşar <dev.muratyasar@gmail.com>
2025-09-08 08:56:35 +0200

    Laravel: request object

--- Coursera/Packt/Laravel/workopia/routes/web.php
+++ Coursera/Packt/Laravel/workopia/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,10 +11,39 @@ Route::get('/jobs', function () {
     return '<h1>Vacant positions</h1>';
 })->name('jobs');
 
-Route::get('/posts/{id}', function (string $id) {
-    return 'Post: ' . $id;
-})->where('id', '[0-9]+');
-
-Route::get('/posts/{id}/comments/{commentId}', function (string $id, string $commentId) {
-    return 'Post: ' . $id . '<br>' . 'comment: ' . $commentId;
+Route::get('/test', function (Request $request) {
+    return [
+        'method' => $request->method(),
+        'url' => $request->url(),
+        'fullUrl' => $request->fullUrl(),
+        'path' => $request->path(),
+        'ip' => $request->ip(),
+        'userAgent' => $request->userAgent(),
+        'header' => $request->header(),
+    ];
 });
+
+
+// Route::get('/users', function (Request $request) {
+//     return $request->query('name');
+// });
+
+// Route::get('/users', function (Request $request) {
+//     return $request->only(['name', 'age']);
+// });
+
+Route::get('/users', function (Request $request) {
+    return $request->all([]);
+});
+
+// Route::get('/users', function (Request $request) {
+//     return $request->has('name');
+// });
+
+// Route::get('/users', function (Request $request) {
+//     return $request->input('birth');
+// });
+
+// Route::get('/users', function (Request $request) {
+//     return $request->except('age');
+// });

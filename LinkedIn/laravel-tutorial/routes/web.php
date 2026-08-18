f5bfe564eb9248e1f4a942c375655298d45d8516
Murat Yaşar <dev.muratyasar@gmail.com>
2026-08-18 16:53:52 +0200

    add WelcomeController

--- LinkedIn/laravel-tutorial/routes/web.php
+++ LinkedIn/laravel-tutorial/routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [WelcomeController::class, 'index']);
 
 Route::get('/home', function () {
     return view('home', ['name' => "Murat"]);
